103a35667784e20d5e8b9e00ab4894f04a9e2bed
JAYARAMGITHUB <35127780+JAYARAMGITHUB@users.noreply.github.com>
2018-01-10 16:09:33 +0530

    Create vwl.c

---
+++ vwl.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+
+char main()
+{
+ char d;
+	printf("enter the character" );
+	scanf("%c",&d);
+	if(d=='a'||d=='e'||d=='i'||d=='o'||d=='u')
+	{
+    printf(" its a vowel character");
+	}
+	else
+	{
+	printf("its a consonant character");
+	}
+}
+		
