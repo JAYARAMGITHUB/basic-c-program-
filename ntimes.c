6b68737038acf69140ea567a91e13108a1cb7da8
JAYARAMGITHUB <35127780+JAYARAMGITHUB@users.noreply.github.com>
2018-01-19 16:09:48 +0530

    Create ntimes.c

---
+++ ntimes.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+void main()
+{
+    int i, n;
+    printf("enter the number of times that u want to display hello word");
+    scanf("%d",n);
+    for (i=0; i<n; i++)
+    { printf("hello\n");
+    }
+   
+}
