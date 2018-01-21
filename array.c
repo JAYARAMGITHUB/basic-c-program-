ac94aada8e04c145c811f7de37c067b1854de34b
JAYARAMGITHUB <35127780+JAYARAMGITHUB@users.noreply.github.com>
2018-01-21 03:59:37 +0000

    Create array.c

---
+++ array.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+void main()
+{
+int n,k,o,p,sum=0;
+printf("enter the value to be inserted in the array");
+scanf("%d\t%d",&n,&k);
+for(o=1;o<=n;o++)
+{
+printf("%d\n",n);
+}
+for(p=1;p<=k;p++)
+{
+sum=sum+j;
+}
+printf("sum is %d",sum);
+}
